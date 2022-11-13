49c19e0f2a52b1f7db86cfcaab0900c02c1cb895
Everule <42273932+overrule@users.noreply.github.com>
2022-11-13 15:59:52 -0600

    Update freq_map.cpp

--- freq_map.cpp
+++ freq_map.cpp
@@ -3,7 +3,7 @@ namespace std::ranges{
 template<ranges::range R>
 auto _freq_map(R&& r){
     using T = std::remove_reference_t<decltype(*r.begin())>;
-    map<T, int> __freq_map;
+    std::map<T, int> __freq_map;
     auto iter_rg = r.begin();
     while(iter_rg != r.end()){
         __freq_map[*iter_rg]++;
@@ -21,5 +21,6 @@ template <ranges::range R>
 constexpr auto operator | (R&& r, const freq_map_range_adapter& adapter){
     return adapter(std::forward<R>(r));
 }
+//Returns std::map<T, int>, where map[x] returns the number of times x exists in the input range
 freq_map_range_adapter freq_map;
 }
