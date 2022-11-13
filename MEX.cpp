91d2b63ae99f828e20cc1d089927e923081b4db4
Everule <42273932+overrule@users.noreply.github.com>
2022-11-13 15:55:35 -0600

    Update MEX.cpp

--- MEX.cpp
+++ MEX.cpp
@@ -1,4 +1,7 @@
-auto MEX(ranges::viewable_range auto&& rg){
+//Input : A range with integer values
+//Output : The smallest non-negative integer not contained in range
+auto MEX(ranges::range auto&& rg){
+    static_assert(is_integral<std::remove_reference_t<decltype(*rg.begin())>);
     int sz = ranges::size(rg);
     vector<char> exists(sz);
     for(auto &x : rg) if(x < sz && x >= 0) exists[x] = 1;
