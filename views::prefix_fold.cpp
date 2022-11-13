2731c17bd7722d549ab6f97111b883f362abda1e
Everule <42273932+overrule@users.noreply.github.com>
2022-11-13 10:17:23 -0600

    Update views::prefix_fold.cpp

--- views::prefix_fold.cpp
+++ views::prefix_fold.cpp
@@ -1,45 +1,32 @@
-namespace std::ranges::views{
+namespace std::ranges{
 
-template<ranges::range R, typename T> requires ranges::view<R> 
-struct prefix_fold_view{
+template<ranges::range R, typename T>
+auto _prefix_fold(R r, auto&& f, T initial){
     vector<T> __prefix_fold;
-    prefix_fold_view(R base, auto&& f, T initial){
-        __prefix_fold.resize(ranges::size(base) + 1);
-        __prefix_fold[0] = initial;
-        auto iter_rg = base.begin();
-        auto iter_fold = __prefix_fold.begin();
-        while(iter_rg != base.end()){
-            *(++iter_fold) = f(*iter_fold, *iter_rg);
-            ++iter_rg;
-        }
+    __prefix_fold.resize(ranges::size(r) + 1);
+    __prefix_fold[0] = initial;
+    auto iter_rg = r.begin();
+    auto iter_fold = __prefix_fold.begin();
+    while(iter_rg != r.end()){
+        *(++iter_fold) = f(*iter_fold, *iter_rg);
+        ++iter_rg;
     }
-    auto begin(){
-        return __prefix_fold.cbegin();
-    }
-    auto end(){
-        return __prefix_fold.cend();
-    }
-    const int& operator[](int x){
-        return __prefix_fold[x];
-    }
-    int size(){
-        return __prefix_fold.size();
-    }
-};
+    return __prefix_fold; 
+}
 template<typename T, typename F>
 struct prefix_fold_range_adapter_closure{
     T initial;
     F f;
     template<ranges::viewable_range R>
     constexpr auto operator()(R&& r){
-        return prefix_fold_view<ranges::views::all_t<R>, T>(r, std::move(f), initial);
+        return _prefix_fold<ranges::views::all_t<R>, T>(r, std::move(f), initial);
     }
     prefix_fold_range_adapter_closure(F&& _f, T _initial) : f(_f), initial(_initial) {}
 };
 struct prefix_fold_range_adapter{
     template<ranges::viewable_range R>
     constexpr auto operator()(R&& r, auto&& f, auto initial) const{
-        return prefix_fold_view<ranges::views::all_t<R>, decltype(initial)>(r, std::move(f), initial);
+        return _prefix_fold<ranges::views::all_t<R>, decltype(initial)>(r, std::move(f), initial);
     }
     constexpr auto operator()(auto &&f, auto initial) const{
         return prefix_fold_range_adapter_closure(std::forward<decltype(f)>(f), initial);
